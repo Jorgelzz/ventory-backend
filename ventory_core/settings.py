cfe91e6d9f9e7371e86bbecca448156ba653c65b
jorgelzz <jorgeluiz2027@hotmail.com>
2025-09-18 13:13:35 -0300

    modified:   Pipfile 	modified:   Pipfile.lock 	modified:   ventory_core/settings.py 	modified:   ventory_core/urls.py

--- ventory_core/settings.py
+++ ventory_core/settings.py
@@ -135,3 +135,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Venture Inventory API",
+    "DESCRIPTION": "API for managing inventory items.",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
